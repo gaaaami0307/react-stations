3419e89e66e8bbad8de25411650ac8bd5ebd054d
Gami <s1320016@u-aizu.ac.jp>
2025-03-04 17:04:00 +0900

    station5 first commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,17 @@ import { useState } from 'react'
 export const App = () => {
   const [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_442.jpg");
 
+  function chgUrl(){
+    queryAllByAttribute("test");
+    //setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg");
+  }
+
   return (
     <>
     <header> Cat APP </header> 
     <h1>犬の画像を表示するサイトです</h1>
     <img src={dogUrl}></img>
+    <button onClick="chgUrl">更新</button>
     </>
     
   )
