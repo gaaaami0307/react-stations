6bac0c2aa753c33fcc583ff9f6cf48d30ac39ebc
Gami <s1320016@u-aizu.ac.jp>
2025-03-04 19:09:02 +0900

    fix:Description

--- src/Description.jsx
+++ src/Description.jsx
@@ -1,28 +1,28 @@
 // @ts-check
 import { useState } from 'react'
 
-const [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_442.jpg");
-
-async function getUrlData(){
-  const URL="https://dog.ceo/api/breeds/image/random";
-  try{
-    const response = await fetch(URL);
-    if(!response.ok) throw new Error(`Error: status-${response.status}`);
-
-    const json = await response.json();
-    return json;
-  } catch(error){
-    console.error(error.message);
-  }
-}
-
-async function chgUrl(){
-  const data = await getUrlData();
-  console.log(data.message);
-  setDogUrl(data.message);
-}
-
 export const Description = () => {
+  const [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_442.jpg");
+
+  async function getUrlData(){
+    const URL="https://dog.ceo/api/breeds/image/random";
+    try{
+      const response = await fetch(URL);
+      if(!response.ok) throw new Error(`Error: status-${response.status}`);
+
+      const json = await response.json();
+      return json;
+    } catch(error){
+      console.error(error.message);
+    }
+  }
+
+  async function chgUrl(){
+    const data = await getUrlData();
+    console.log(data.message);
+    setDogUrl(data.message);
+  }
+
 
   return <>
     <h1>犬の画像を表示するサイトです</h1>
