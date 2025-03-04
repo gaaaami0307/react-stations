6518c2fc4bfede9908cf4335b7e888f554e8a114
Gami <s1320016@u-aizu.ac.jp>
2025-03-04 19:07:03 +0900

    station08 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
 // DO NOT DELETE
 
 import './App.css'
-import { useState } from 'react'
 import { Header } from "./Header"
+import { Description } from "./Description"
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
-  const [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_442.jpg");
-
-  async function getUrlData(){
-    const URL="https://dog.ceo/api/breeds/image/random";
-    try{
-      const response = await fetch(URL);
-      if(!response.ok) throw new Error(`Error: status-${response.status}`);
-
-      const json = await response.json();
-      return json;
-    } catch(error){
-      console.error(error.message);
-    }
-  }
-
-  async function chgUrl(){
-    const data = await getUrlData();
-    console.log(data.message);
-    setDogUrl(data.message);
-  }
-
   return (
     <>
     <Header/>
-    <h1>犬の画像を表示するサイトです</h1>
-    <img className="updatebtn" src={dogUrl}></img>
-    <button onClick={chgUrl}>更新</button>
+    <Description/>
     </>
     
   )
--- src/Description.jsx
+++ src/Description.jsx
@@ -1,7 +1,34 @@
 // @ts-check
+import { useState } from 'react'
+
+const [dogUrl,setDogUrl] = useState("https://images.dog.ceo/breeds/elkhound-norwegian/n02091467_442.jpg");
+
+async function getUrlData(){
+  const URL="https://dog.ceo/api/breeds/image/random";
+  try{
+    const response = await fetch(URL);
+    if(!response.ok) throw new Error(`Error: status-${response.status}`);
+
+    const json = await response.json();
+    return json;
+  } catch(error){
+    console.error(error.message);
+  }
+}
+
+async function chgUrl(){
+  const data = await getUrlData();
+  console.log(data.message);
+  setDogUrl(data.message);
+}
 
 export const Description = () => {
-  return <></>
+
+  return <>
+    <h1>犬の画像を表示するサイトです</h1>
+    <img className="updatebtn" src={dogUrl}></img>
+    <button onClick={chgUrl}>更新</button>
+  </>
 }
 
 export default Description
